9c4f7644a6c2ba185c7c6af066a9377f368e9d4d
Ayesha Ushani <akisasenari@gmail.com>
2025-06-18 12:01:59 +0530

    create main service modules

---
+++ User_service/src/main/java/org/example/user_service/dto/UserRequest.java
@@ -0,0 +1,13 @@
+package org.example.user_service.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private String role;
+}
+
